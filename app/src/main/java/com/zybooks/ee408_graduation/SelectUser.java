22acb251f47673f17c24edfa5db1893a4f45d96d
John <fontaijg@clarkson.edu>
2022-11-23 17:04:38 -0500

    Changed layouts to work for multiple phones. Made Existing Profile Selection Radio Buttons work.

--- app/src/main/java/com/zybooks/ee408_graduation/SelectUser.java
+++ app/src/main/java/com/zybooks/ee408_graduation/SelectUser.java
@@ -4,16 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioGroup;
 
 public class SelectUser extends AppCompatActivity {
     private Button selectBack;
+    private RadioGroup rg1;
+    private RadioGroup rg2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_user);
 
+        rg1 = (RadioGroup) findViewById(R.id.radioGroup);
+        rg2 = (RadioGroup) findViewById(R.id.radioGroup2);
+        rg1.clearCheck(); // this is so we can start fresh, with no selection on both RadioGroups
+        rg2.clearCheck();
+        rg1.setOnCheckedChangeListener(listener1);
+        rg2.setOnCheckedChangeListener(listener2);
         selectBack = (Button) findViewById(R.id.selectBack);
         selectBack.setOnClickListener(view -> returnToMainPage());
     }
@@ -22,4 +34,31 @@ public class SelectUser extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
+
+    // The following section of code was taken from https://stackoverflow.com/questions/10425569/radiogroup-with-two-columns-which-have-ten-radiobuttons
+    private RadioGroup.OnCheckedChangeListener listener1 = new RadioGroup.OnCheckedChangeListener() {
+
+        @Override
+        public void onCheckedChanged(RadioGroup group, int checkedId) {
+            if (checkedId != -1) {
+                rg2.setOnCheckedChangeListener(null); // remove the listener before clearing so we don't throw that stackoverflow exception(like Vladimir Volodin pointed out)
+                rg2.clearCheck(); // clear the second RadioGroup!
+                rg2.setOnCheckedChangeListener(listener2); //reset the listener
+                Log.e("XXX2", "do the work");
+            }
+        }
+    };
+
+    private RadioGroup.OnCheckedChangeListener listener2 = new RadioGroup.OnCheckedChangeListener() {
+
+        @Override
+        public void onCheckedChanged(RadioGroup group, int checkedId) {
+            if (checkedId != -1) {
+                rg1.setOnCheckedChangeListener(null);
+                rg1.clearCheck();
+                rg1.setOnCheckedChangeListener(listener1);
+                Log.e("XXX2", "do the work");
+            }
+        }
+    };
 }
