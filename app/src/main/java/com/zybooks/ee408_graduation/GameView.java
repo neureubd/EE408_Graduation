6f07b06d4cddb15cbd66d8ce72ecfe8e8df34398
Dalton OKeefe <daltonjokeefe@gmail.com>
2022-11-28 15:56:51 -0500

    game back button

--- app/src/main/java/com/zybooks/ee408_graduation/GameView.java
+++ app/src/main/java/com/zybooks/ee408_graduation/GameView.java
@@ -2,14 +2,34 @@ package com.zybooks.ee408_graduation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GameView extends AppCompatActivity {
-
+    private Button gameBack;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_view);
+
+        gameBack = (Button) findViewById(R.id.gameBack);
+
+        gameBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                returnToMainPage();
+            }
+        });
+        
+    }
+
+    public void returnToMainPage(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
 //Test. - Dalton
