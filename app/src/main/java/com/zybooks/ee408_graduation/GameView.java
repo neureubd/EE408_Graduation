9cbd947b89dbaa7ab77d9a3e4333369280a6de52
Dalton OKeefe <daltonjokeefe@gmail.com>
2022-11-30 16:42:30 -0500

    fixed game view relative layout and added button to start game

--- app/src/main/java/com/zybooks/ee408_graduation/GameView.java
+++ app/src/main/java/com/zybooks/ee408_graduation/GameView.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 
 public class GameView extends AppCompatActivity {
     private Button gameBack;
+    private Button gameStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +18,14 @@ public class GameView extends AppCompatActivity {
         setContentView(R.layout.activity_game_view);
 
         gameBack = (Button) findViewById(R.id.gameBack);
+        gameStart = (Button) findViewById(R.id.gameStart);
+
+        gameStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
         gameBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,3 +42,13 @@ public class GameView extends AppCompatActivity {
     }
 }
 //Test. - Dalton
+/*
+* Notes for making the game
+* on start button summon book and arrow
+* when swipe book correct arrow gone
+* summon broken book
+* summon book and new arrow
+* new book appear
+* repeat...
+* when final number == goal number end game
+* */
