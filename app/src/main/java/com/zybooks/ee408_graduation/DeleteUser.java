704925bfdb660cc566fe3e414542474ed2ff7e3a
Bradley Neureuther <neureubd@clarkson.edu>
2022-12-02 01:00:20 -0500

    Implemented the API in DeleteUser. In summery we are not able to create, select, delete, and send data using the API

--- app/src/main/java/com/zybooks/ee408_graduation/DeleteUser.java
+++ app/src/main/java/com/zybooks/ee408_graduation/DeleteUser.java
@@ -7,12 +7,22 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.android.volley.VolleyError;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class DeleteUser extends AppCompatActivity {
     private Button deleteBack;
+    private Button deleteButton;
     private RadioGroup rg1;
     private RadioGroup rg2;
+    String baseUrl = "http://10.0.2.2:5000/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +36,11 @@ public class DeleteUser extends AppCompatActivity {
         rg1.setOnCheckedChangeListener(listener1);
         rg2.setOnCheckedChangeListener(listener2);
         deleteBack = (Button) findViewById(R.id.deleteBack);
+        deleteButton = (Button) findViewById(R.id.deleteButton);
+        getUserList();
         deleteBack.setOnClickListener(view -> returnToMainPage());
+        deleteButton.setOnClickListener(view -> checkForUser());
 
-        //deleteBack = findViewById(R.id.deleteBack);
-
-        //deleteBack.setOnClickListener(view -> returnToMainPage());
     }
 
     public void returnToMainPage(){
@@ -62,4 +72,78 @@ public class DeleteUser extends AppCompatActivity {
             }
         }
     };
+
+    public void getUserList(){ //rename
+        String[] nameList = new String[6];
+        String baseUrl = "http://10.0.2.2:5000/";
+        new ApiRequest(DeleteUser.this, baseUrl + "get_usernames", new JSONObject()) {
+            @Override
+            public void PostCallback(JSONObject jsonObject, VolleyError volleyError) {
+                JSONArray names = jsonObject.optJSONArray("users");
+                for(int i=0;i < 6; i++) {
+                    if(i < names.length()) {nameList[i] = names.optString(i);}
+                    else{nameList[i] = "";}
+                }
+                setRadioButtonText(nameList);
+            }
+        };
+    }
+
+    public void setRadioButtonText(String[] nameList){
+        // Profile 1
+        if (nameList[0] != "") {
+            RadioButton b1 = (RadioButton) rg1.getChildAt(0);b1.setText(nameList[0]);}
+        else{((RadioButton) rg1.getChildAt(0)).setText("Profile 1");}
+        //Profile 2
+        if (nameList[1] != "") {((RadioButton) rg2.getChildAt(0)).setText(nameList[1]);}
+        else{((RadioButton) rg2.getChildAt(0)).setText("Profile 2");}
+        // Profile 3
+        if (nameList[2] != "") {((RadioButton) rg1.getChildAt(1)).setText(nameList[2]);}
+        else{((RadioButton) rg1.getChildAt(1)).setText("Profile 3");}
+        // Profile 4
+        if (nameList[3] != "") {((RadioButton) rg2.getChildAt(1)).setText(nameList[3]);}
+        else{((RadioButton) rg2.getChildAt(1)).setText("Profile 4");}
+        // Profile 5
+        if (nameList[4] != "") {((RadioButton) rg1.getChildAt(2)).setText(nameList[4]);}
+        else{((RadioButton) rg1.getChildAt(2)).setText("Profile 5");}
+        // Profile 6
+        if (nameList[5] != "") {((RadioButton) rg2.getChildAt(2)).setText(nameList[5]);}
+        else{((RadioButton) rg2.getChildAt(2)).setText("Profile 6");}
+
+    }
+
+    public void checkForUser(){
+        RadioButton rb = null;
+
+        if(rg1.getCheckedRadioButtonId() != -1){rb = (RadioButton) findViewById(rg1.getCheckedRadioButtonId());}
+        else if(rg2.getCheckedRadioButtonId() != -1){rb = (RadioButton) findViewById(rg2.getCheckedRadioButtonId());}
+        else{
+            Toast.makeText(getApplicationContext(), "No User Selected",Toast.LENGTH_LONG).show();
+            return;
+        }
+        // Check for user
+        if (rb.getText().toString().contains("Profile")) {
+            Toast.makeText(getApplicationContext(), "This is an inactive profile",Toast.LENGTH_LONG).show();
+            return;
+        } else {
+            deleteProfile((String) rb.getText());
+        }
+    }
+
+    public void deleteProfile(String newUserName){
+        JSONObject jsonParams = new JSONObject();
+        try {
+            jsonParams.put("username", newUserName);
+            new ApiRequest(DeleteUser.this, baseUrl + "delete_user", jsonParams) {
+                @Override
+                public void PostCallback(JSONObject jsonObject, VolleyError volleyError) {
+                    String errString = jsonObject.optString("error");
+                    //Toast.makeText(EnrollNewUser.this.getApplicationContext(), "Error: " + errString, Toast.LENGTH_LONG).show();
+                }
+            };
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+        getUserList();
+    }
 }
